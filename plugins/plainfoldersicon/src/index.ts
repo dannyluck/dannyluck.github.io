947baf1a7f2340f140319face7287b8762a03db2
Danny <67912255+dannyluck@users.noreply.github.com>
2024-05-31 21:09:11 +0200

    Update index.ts

--- plugins/plainfoldersicon/src/index.ts
+++ plugins/plainfoldersicon/src/index.ts
@@ -1,1 +1,15 @@
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
 
+export default definePlugin({
+    name: "PlainFolderIcon",
+    description: "Doesn't show the small guild icons in folders",
+    authors: [Devs.botato],
+    patches: [{
+        find: ".expandedFolderIconWrapper",
+        replacement: [{
+            match: /\(\w\|\|\w\)&&(\(.{0,40}\(.{1,3}\.animated)/,
+            replace: "$1",
+        }]
+    }]
+});
