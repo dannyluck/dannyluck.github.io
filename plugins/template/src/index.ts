5d66be5377b8742d7116fd41f9f26b0422d02651
Danny <67912255+dannyluck@users.noreply.github.com>
2024-05-31 21:04:53 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,15 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+export default definePlugin({
+    name: "PlainFolderIcon",
+    description: "Doesn't show the small guild icons in folders",
+    authors: [Devs.botato],
+    patches: [{
+        find: ".expandedFolderIconWrapper",
+        replacement: [{
+            match: /\(\w\|\|\w\)&&(\(.{0,40}\(.{1,3}\.animated)/,
+            replace: "$1",
+        }]
+    }]
+});
